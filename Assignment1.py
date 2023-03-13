74c84930a92dd924c2169fc71e40c141cecf1dfb
wadegoren <79055042+wadegoren@users.noreply.github.com>
2023-03-13 18:59:20 -0400

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -7,19 +7,19 @@ runTime = int(1000000)
 finalArray = []
 #Simulate the queue arrival rate and calculate the expected delay
 for arrivalRate in arrivalRates:
- queue = []
- time = 0
- numArrived = 1
- for i in range(runTime):
- if arrivalRate > np.random.random():
- queue.append(np.random.geometric(serviceRate))
- if 0 < len(queue):
- time = time + len(queue) - 1
- queue[0] -= 1
- if queue[0] == 0:
- queue.pop(0)
- numArrived += 1
- expectedDelay = time/numArrived
+  queue = []
+  time = 0
+  numArrived = 1
+  for i in range(runTime):
+   if arrivalRate > np.random.random():
+    queue.append(np.random.geometric(serviceRate))
+   if 0 < len(queue):
+    time = time + len(queue) - 1
+    queue[0] -= 1
+    if queue[0] == 0:
+     queue.pop(0)
+     numArrived += 1
+   expectedDelay = time/numArrived
  finalArray.append(expectedDelay)
 #Plot the expeected queueing delay with respect to the arrival rate
 plt.plot(arrivalRates, finalArray)
